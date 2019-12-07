80f37ecafa0ffa0f99a0179b7604a5f3c0887086
AislynnEdmiston <davidandaislynn@gmail.com>
2019-12-07 14:19:02 -0500

    inported welcome and changed port 5000 to 4000

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 
+// routers
+const welcomeRouter = require('./routers/welcome')
+
 const server = express()
 
 server.use(express.json())
+server.use('/', welcomeRouter)
 
-server.listen(5000, () => {
-    console.log('\n*** Server is running on https://localhost:5000 ***\n')
+server.listen(4000, () => {
+    console.log('\n*** Server is running on https://localhost:4000 ***\n')
 })
