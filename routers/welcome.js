985f69b69eebc217ecc1b0d1e16a0d26c45142a2
AislynnEdmiston <davidandaislynn@gmail.com>
2019-12-07 14:10:10 -0500

    created the base routes with messages

---
+++ routers/welcome.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('<h2>Please go to "http://localhost:5000/api/posts"</h2>')
+})
+
+router.get('/api', (req, res) => {
+    res.json({message: 'Welcome to the API! Please go to "http://localhost:5000/api/posts" to get started!'})
+})
+
+module.exports = router
